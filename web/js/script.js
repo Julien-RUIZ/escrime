7f599fafdcaf0cd6d41e410f5da222021262b171
lapiscine <julien.ruiz@lapiscine.pro>
2019-01-10 17:00:52 +0100

    J'ai fait le responsive en telephone et tablette, voir en 1920x1080, début du javascript

--- web/js/script.js
+++ web/js/script.js
@@ -1,7 +1,36 @@
 
 
 
-console.log("coucou");
+$(document).ready(function(){
+
+    $(".actustitre").mouseover(function(){
+        $(this).css('color','rgb(86,3,121)');
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
 
 
 
