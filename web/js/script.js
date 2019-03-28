db33ea0c025ad1098060983931906402a2c071a7
lapiscine <julien.ruiz@lapiscine.pro>
2019-03-28 11:37:02 +0100

    Mise a jour premiere page, design et liens vers les autres pages + javascript

--- web/js/script.js
+++ web/js/script.js
@@ -3,24 +3,87 @@
 
 $(document).ready(function(){
 
+
+
+    //-------------------------------------------
     $(".actustitre").mouseover(function(){
         $(this).css('color','rgb(86,3,121)');
     })
 
-
 $(".texteactus1").click(function(){
     $(this).css('color-webkit-line-clamp','0');
 })
+    //-------------------------------------------
 
 
 
+    $(".imageUne").mouseover(function(){
+        $(".titreUn").css('fontSize','22px');
+    })
+    $(".imageUne").mouseout(function(){
+
+        $(".titreUn").css('fontSize','16px');
+    })
+//-------------------------------------------
+    $(".imageDeux").mouseover(function(){
+
+        $(".titreDeux").css('fontSize','22px');
+    })
+    $(".imageDeux").mouseout(function(){
+
+        $(".titreDeux").css('fontSize','16px');
+    })
+//-------------------------------------------
+    $(".imageTrois").mouseover(function(){
+
+        $(".titreTrois").css('fontSize','22px');
+    })
+    $(".imageTrois").mouseout(function(){
+
+        $(".titreTrois").css('fontSize','16px');
+    })
+//-------------------------------------------
+    $(".imageQuatre").mouseover(function(){
+
+        $(".titreQuatre").css('fontSize','22px');
+    })
+    $(".imageQuatre").mouseout(function(){
+
+        $(".titreQuatre").css('fontSize','16px');
+    })
+
+    //--------------------------------------------
+    $(".LienDansImage").hide();
+    $(".LienDansImage1").hide();
+    $(".LienDansImage2").hide();
+    $(".LienDansImage3").hide();
 
 
 
+    $("div.borderGeneric").click(function(){
+        if($(this).hasClass('imageUne')) {
 
 
+        }
+        else if ($(this).hasClass('imageDeux')){
+            $(".LienDansImage1").slideToggle();
+            $(".LienDansImage2").slideUp();
+            $(".LienDansImage3").slideUp();
 
 
+        }
+        else if ($(this).hasClass('imageTrois')){
+            $(".LienDansImage2").slideToggle();
+            $(".LienDansImage1").slideUp();
+            $(".LienDansImage3").slideUp();
+
+        }
+        else if ($(this).hasClass('imageQuatre')){
+            $(".LienDansImage3").slideToggle();
+            $(".LienDansImage1").slideUp();
+            $(".LienDansImage2").slideUp();
+
+        }})
 
 
 
